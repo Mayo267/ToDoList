d3b1c8b4866ab0624cb35092c05353202d74a849
Andrew <amayo0925@gmail.com>
2019-07-05 19:29:34 -0400

    lots of additions

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 
 
 function addTodo() {
+    event.preventDefault();
+
     var item = document.getElementById("todoInput").value;
-    var text = document.createTextNode(item);
+    // var text = document.createTextNode(item);
     var newItem = document.createElement("li");
     newItem.className = "createdItem";
-    newItem.appendChild(text);
-    document.getElementById("todoList").appendChild(newItem);
-
+    // newItem.appendChild(text);
+    // document.getElementById("todoList").appendChild(newItem);
+    $("#todoList").append("<div class = 'newestItem' >" + "❧" + " " +  item + "<hr>" +"</div>");
     //Clearing the input button when submitted...
-    var input = document.getElementById("todoInput");
-    input.value = '';
+    $("#todoInput").val('');
+
     
-    //If the form is empty, don't submit...
-    if(input.value == '') {
-        //?Reject the submissions...
-    }
+    //TODO==================================================
+    //!If the form is empty, don't submit...
+    // if(input.value == '') {
+    //     //?Reject the submissions...
+    // }
+    //TODO==================================================
+    
 };
 
 
+
+$("#todoInput").keypress( function(event) {
+    if(event.which == 13 ) {
+        addTodo();
+        return false;
+    }
+})
+
+//Cross out finished tasks...
+function finished() {
+    
+};
+
+//Remove a single task from the list...
 function removeFromList() {
 
-    
 };
 
 //Clearing the entire list of entries...
 document.getElementById("clearButton").addEventListener("click", function(){
     var listItems = document.getElementById("todoList");
-
     listItems.innerHTML = '';
 });
+
